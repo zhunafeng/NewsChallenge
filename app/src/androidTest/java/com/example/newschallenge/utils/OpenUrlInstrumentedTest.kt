92c59206bd4bdda21b0726992b64f17b0e8dc3fd
NafengZhu <96706516+zhunafeng@users.noreply.github.com>
2022-12-17 02:03:09 +1100

    add: ui test

---
+++ app/src/androidTest/java/com/example/newschallenge/utils/OpenUrlInstrumentedTest.kt
@@ -0,0 +1,46 @@
+package com.example.newschallenge.utils
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.newschallenge.news.ui.activity.NewsNineActivity
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class OpenUrlInstrumentedTest {
+
+    @get:Rule
+    lateinit var activityScenarioRule: ActivityScenario<NewsNineActivity>
+
+
+    @Before
+    fun start(){
+        activityScenarioRule = ActivityScenario.launch(startIntent)
+    }
+
+    @Test
+    fun givenWhenClickThenGoingToDetailActivity(){
+
+    }
+
+    companion object {
+
+        val STRING_TO_BE_TYPED = "Espresso"
+
+        lateinit var startIntent: Intent
+        @BeforeClass
+        @JvmStatic
+        fun setup(){
+            startIntent = Intent(ApplicationProvider.getApplicationContext(),NewsNineActivity::class.java)
+            startIntent.putExtra(STRING_TO_BE_TYPED,"url")
+        }
+    }
+
+
+
+}
